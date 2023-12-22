59b3d7c2efe8fc984690aa9e59dda971a3292397
Felipe Rosene Klapsztein <feliperosenek@gmail.com>
2023-12-22 19:49:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const {
 const minimist = require('minimist');
 const params = minimist(process.argv.slice(2))
 
-const sequelize = new Sequelize('eduard72_tecidos', 'eduard72_wp625', '37@S0DSm(p', {
+const sequelize = new Sequelize('eduard72_parana2023', 'eduard72_wp625', '37@S0DSm(p', {
   host: 'sh-pro20.hostgator.com.br',
   dialect: "mysql",
   define: {
@@ -89,7 +89,7 @@ async function validaGoogle() {
 
     for (let i = 0; i < getIndustrias.length; i++) { // loop principal que roda de acordo com o número de indústrias
 
-       const getIndustria = await sequelize.query("SELECT id_catalogo, nome, fantasia FROM `catalogo` WHERE atualizado=0 ORDER BY RAND() LIMIT 1", { // pega as industrias no banco que não estão atualizadas em ordem aleatória
+       const getIndustria = await sequelize.query("SELECT id_catalogo, nome, fantasia FROM `catalogo` WHERE atualizado=0  ORDER BY RAND() LIMIT 1", { // pega as industrias no banco que não estão atualizadas em ordem aleatória
       type: QueryTypes.SELECT
     });
 
@@ -102,13 +102,13 @@ async function validaGoogle() {
 
         console.log(" ");
         console.log(" ");
-        console.log(" 🔎   Buscando informações sobre: " + getIndustria[0].fantasia)
+        console.log(" 🔎   Buscando informações sobre: " + getIndustria[0].nome)
         console.log("Pesquisa " + i + " de " + (getIndustrias.length - 1))
       }
 
         // --> DEFINE A URL DE PESQUISA DE ACORDO COM O NOME DA EMPRESA
 
-      urlSearch = getIndustria[0].nome.replace(/\s/g, "+") // pega o nome da empresa e formata os espaçoes com o caracteres + para ser usado na pesquisa do Google
+      urlSearch = getIndustria[0].nome.replace(/\s/g, "+"); // pega o nome da empresa e formata os espaçoes com o caracteres + para ser usado na pesquisa do Google
 
       page.goto("https://www.google.com.br/search?q=" + urlSearch) //faz a busca com a url formatada
       await delay(4000)
@@ -254,7 +254,7 @@ async function validaGoogle() {
 
       results = {
         id: getIndustria[0].id_catalogo,
-        nome: getIndustria[0].fantasia,
+        nome: getIndustria[0].nome,
         site: linkSite,
         face: facebookLink,
         insta: instagramLink,
@@ -364,7 +364,7 @@ async function validaGoogle() {
           facebookAbout = getDataBase[0].descricao
         }
 
-        await sequelize.query("UPDATE catalogo SET ddd_telefone='" + dddTelefone + "', telefone='" + telGoogle + "', ddd_telefone2='" + dddTelefone2 + "', telefone2='" + telFacebook + "', email='" + getFacebookEmail + "', pagina_web='" + results.site + "', instagram='" + instagramLink + "', facebook='" + facebookLink + "', linkedin='" + linkedinLink + "', descricao='" + facebookAbout + "', emailAtualizado='" + emailAtualizado + "', telefoneAtualizado='" + telefoneAtualizado + "', atualizado=2, google=" + verifyGoogle + "  WHERE id_catalogo = " + getDataBase[0].id_catalogo + "")
+        await sequelize.query("UPDATE catalogo SET ddd_telefone='" + dddTelefone + "', telefone='" + telGoogle + "', ddd_telefone2='" + dddTelefone2 + "', telefone2='" + telFacebook + "', email='" + getFacebookEmail + "', pagina_web='" + results.site + "', instagram='" + instagramLink + "', facebook='" + facebookLink + "', linkedin='" + linkedinLink + "', descricao='" + facebookAbout + "', emailAtualizado='" + emailAtualizado + "', telefoneAtualizado='" + telefoneAtualizado + "', atualizado=1, google=" + verifyGoogle + "  WHERE id_catalogo = " + getDataBase[0].id_catalogo + "")
         console.log('  ✔  Atualizado ');
 
       }
